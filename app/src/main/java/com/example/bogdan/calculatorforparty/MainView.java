b6aab89e32728830e1a69e17b1e128b046e83fc3
Bogdan Kolomiets <kolom996@gmail.com>
2016-08-07 14:41:30 +0300

    #2 Adding View Interface

---
+++ app/src/main/java/com/example/bogdan/calculatorforparty/MainView.java
@@ -0,0 +1,12 @@
+package com.example.bogdan.calculatorforparty;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 07.08.16
+ */
+public interface MainView {
+  void addNewPerson();
+  void calculateAmountPerPerson();
+  void calculateWhoIsWho();
+}
